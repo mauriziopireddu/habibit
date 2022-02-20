f68608f94e80fc54e7b483ef2487977d8c522299
Maurizio Pireddu <pireddu.maurizio@gmail.com>
2022-02-20 21:05:20 +0000

    add navigation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,8 @@
-import { getDaysInMonth } from 'date-fns'
+import { add, getDaysInMonth, sub } from 'date-fns'
 import { json, LoaderFunction, useLoaderData } from 'remix'
 import languageParser from 'accept-language-parser'
 import { daysOfWeek, getOffsetFromFirstMonday } from '~/utils/dates'
+import { useState } from 'react'
 
 type LoaderData = { locale: string }
 
@@ -21,39 +22,67 @@ export let loader: LoaderFunction = async ({ request }) => {
 
 export default function Index() {
   const { locale } = useLoaderData<LoaderData>()
-
+  const [date, setDate] = useState(new Date())
   const onClick = () => {}
-  const date = new Date()
   const offset = getOffsetFromFirstMonday(date)
 
   return (
     <main>
       <section className="px-4">
-        <h2 className="text-3xl text-center uppercase">
-          {new Intl.DateTimeFormat(locale, {
-            year: 'numeric',
-            month: 'long',
-          }).format(date)}
-        </h2>
-        <div className="grid grid-cols-7 uppercase text-center my-2">
-          {daysOfWeek(locale).map(day => (
-            <div key={day} className="">
-              {day}
-            </div>
-          ))}
-        </div>
-        <div className="grid grid-cols-7 gap-1">
-          {[...Array(getDaysInMonth(date))].map((day, index) => (
+        <div className="border-[1px] shadow-sm p-5 my-2">
+          <h2 className="text-3xl text-center uppercase">
+            {new Intl.DateTimeFormat(locale, {
+              year: 'numeric',
+              month: 'long',
+            }).format(date)}
+          </h2>
+          <div className="text-center">
             <button
-              key={index}
-              onClick={onClick}
-              className={`first:col-start-${
-                offset + 1
-              } aspect-square border-[1px]`}
+              type="button"
+              aria-label="Go to previous month"
+              className="text-2xl"
+              onClick={() => setDate(sub(date, { months: 1 }))}
             >
-              <time dateTime={`2022-02-${index}`}>{index + 1}</time>
+              ⬅
             </button>
-          ))}
+            <span className="mx-2 sp">|</span>
+            <button
+              type="button"
+              aria-label="Go to next month"
+              className="text-2xl"
+              onClick={() => setDate(add(date, { months: 1 }))}
+            >
+              ➡️
+            </button>
+          </div>
+          <div className="grid grid-cols-7 uppercase text-center my-2">
+            {daysOfWeek(locale).map(day => (
+              <div key={day} className="">
+                {day}
+              </div>
+            ))}
+          </div>
+          <div className="grid grid-cols-7 gap-1">
+            {[...Array(getDaysInMonth(date))].map((day, index) => (
+              <button
+                key={index}
+                onClick={onClick}
+                className={`first:col-start-${
+                  offset + 1
+                } aspect-square border-[1px] hover:bg-slate-100`}
+              >
+                <time
+                  dateTime={new Date(
+                    date.getFullYear(),
+                    date.getMonth(),
+                    index + 1,
+                  ).toLocaleDateString(locale)}
+                >
+                  {index + 1}
+                </time>
+              </button>
+            ))}
+          </div>
         </div>
       </section>
     </main>
